8fb7e286cfd2b8204615993bfe7ceaaa50413e56
ankitrraj <ankitrajoria81@gmail.com>
2025-11-30 19:43:32 +0530

    firstproject

---
+++ projects/color_changer/script.js
@@ -0,0 +1,37 @@
+const selectbutton = document.querySelectorAll(".button")
+const body = document.querySelector("body")
+
+
+selectbutton.forEach(function (e){
+    // console.log(e)
+    e.addEventListener( 'click',function (btn){
+        // console.log(btn);
+        console.log(btn.target);
+        if (btn.target.id ==="yellow") {
+            body.style.backgroundColor = btn.target.id;
+        
+        }
+        if (btn.target.id ==="red") {
+            body.style.backgroundColor = btn.target.id;
+        
+        }
+        if (btn.target.id ==="green") {
+            body.style.backgroundColor = btn.target.id;
+        
+        }
+        if (btn.target.id ==="blue") {
+            body.style.backgroundColor = btn.target.id;
+        
+        }
+
+   
+        
+
+
+
+
+
+    });
+    
+
+});
