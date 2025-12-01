9969770732bc03ccd9bd17cf59626439e1a4d491
ankitrraj <ankitrajoria81@gmail.com>
2025-12-01 23:24:49 +0530

    project 2 bmi calculator

---
+++ projects/bmi_calculator/index.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit',function (e){
+    // console.log(e);
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    console.log(height);
+    
+    const weight = parseInt(document.querySelector('#weight').value)
+    console.log(weight);
+    
+    const result = document.querySelector('.result')
+
+    if(height ===''||height<0||isNaN(height)){
+        result.innerHTML = `enter the vaild height ${height}`
+
+    }else if(height ===''||height<0||isNaN(height)){
+        result.innerHTML = `enter the vaild height ${height}`
+
+    }else{
+        const bmi = (weight/(height**2/10000)).toFixed(3)
+
+        result.innerHTML = `<span>${bmi}</span>`
+    }
+
+    
+    
+   
+    
+})
